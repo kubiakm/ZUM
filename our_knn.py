ad609f296fe502f4c6c5988905d4dababe1f1c7a
mati9456 <mati9456@gmail.com>
2024-06-09 16:06:08 +0200

    semi final version

--- our_knn.py
+++ our_knn.py
@@ -3,11 +3,11 @@ import numba as nb
 
 
 class KNNAnomalyDetector:
-    def __init__(self, k=5, threshold_percentile=95, distance_func=None, decision_func=None, external_threshold=None):
+    def __init__(self, k=5, threshold_percentile=95, distance_func=None, score_func=None, external_threshold=None):
         self.k = k
         self.threshold_percentile = threshold_percentile
         self.distance_func = distance_func if distance_func is not None else self.default_distance
-        self.decision_func = decision_func if decision_func is not None else self.decision_function
+        self.score_func = score_func if score_func is not None else self.default_score
         self.external_threshold = external_threshold
         self.threshold_ = None
         self.anomaly_scores_ = None
@@ -19,8 +19,7 @@ class KNNAnomalyDetector:
         
         distances = self.calc_distances(self.n_samples, self.X_train, self.distance_func)
 
-        nearest_neighbors = np.argsort(distances, axis=1)[:, 1:self.k+1]
-        self.anomaly_scores_ = np.mean(distances[np.arange(self.n_samples)[:, None], nearest_neighbors], axis=1)
+        self.anomaly_scores_ = self.score_func(distances, self.X_train, self.k)
 
         # if there's no threshold given, we determine treshold using percentile
         if self.external_threshold is None:
@@ -49,28 +48,32 @@ class KNNAnomalyDetector:
         
         distances = self.calc_distances_pred(self.X_train, X, self.distance_func)
 
-        # default decition is based on mean distance to k nearest neighbors
-        nearest_neighbors = np.argsort(distances, axis=1)[:, :self.k]
-        scores = np.mean(distances[np.arange(len(X))[:, None], nearest_neighbors], axis=1)
+        scores = self.score_func(distances, X, self.k)
 
         return scores
+    
+    @staticmethod
+    def default_score(distances, X, k):
+        # default decition is based on mean distance to k nearest neighbors
+        nearest_neighbors = np.argsort(distances, axis=1)[:, :k]
+        return np.mean(distances[np.arange(len(X))[:, None], nearest_neighbors], axis=1)
 
     @staticmethod
-    @nb.jit(nopython=True) 
+    @nb.jit(nopython=True, parallel=True) 
     def calc_distances(n_samples, X, distance_func):
         # compute distance between points
         distances = np.zeros((n_samples, n_samples))
-        for i in range(n_samples):
+        for i in nb.prange(n_samples):
             for j in range(i + 1, n_samples):
                 distances[i, j] = distance_func(X[i], X[j])
                 distances[j, i] = distances[i, j]
         return distances
     
     @staticmethod
-    @nb.jit(nopython=True) 
+    @nb.jit(nopython=True, parallel=True) 
     def calc_distances_pred(X_train, X, distance_func):
         distances = np.zeros((len(X), len(X_train)))
-        for i in range(len(X)):
+        for i in nb.prange(len(X)):
             for j in range(len(X_train)):
                 distances[i, j] = distance_func(X[i], X_train[j])
         return distances
--- test_functions.py
+++ test_functions.py
@@ -190,7 +190,7 @@ def train_for_anomalies_and_non_anomalies(X, y, a_or_na):
     plt.show()
 
 
-def test_loop(X, y, k_list, distances, decisions, n_splits=5, threshold=95, filter_inliers=False):
+def test_loop(X, y, k_list=[5], distances=[None], score_funcs=[None], n_splits=5, thresholds=[95], filter_inliers=[False]):
     X = np.array(X)
     y = np.array(y)
 
@@ -204,44 +204,53 @@ def test_loop(X, y, k_list, distances, decisions, n_splits=5, threshold=95, filt
     k_neighbors_arr = []
     cv_arr = []
     decisions_arr = []
+    filter_arr = []
+    threshold_arr = []
 
-    for k in k_list:
-        for d in distances:
-            for dec in decisions:
-                detector = KNNAnomalyDetector(k=k, distance_func=d, decision_func=dec, threshold_percentile=threshold)
-                for i, (train_index, test_index) in enumerate(skf.split(X, y)):
-                    X_train, X_test = X[train_index], X[test_index]
-                    y_train, y_test = y[train_index], y[test_index]
+    for filter in filter_inliers:
+        for th in thresholds:
+            for k in k_list:
+                for d in distances:
+                    for scr in score_funcs:
+                        detector = KNNAnomalyDetector(k=k, distance_func=d, score_func=scr, threshold_percentile=th)
+                        for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+                            X_train, X_test = X[train_index], X[test_index]
+                            y_train, y_test = y[train_index], y[test_index]
 
-                    # filter only inliers 
-                    if filter_inliers:
-                        inliers_index = np.where(y_train==1)
-                        X_train = X_train[inliers_index]
+                            # filter only inliers 
+                            if filter:
+                                inliers_index = np.where(y_train==1)
+                                X_train = X_train[inliers_index]
 
-                    detector.fit(X_train)
-                    predictions = detector.predict(X_test, detector.threshold_)
+                            detector.fit(X_train)
+                            predictions = detector.predict(X_test, detector.threshold_)
 
-                    scores = detector.decision_function(X_test)
-                    precision, recall, _ = precision_recall_curve(y_test, scores)
-                    pr_auc = auc(recall, precision)
-                    f1 = f1_score(y_test, predictions, average='binary')
+                            scores = detector.decision_function(X_test)
+                            precision, recall, _ = precision_recall_curve(y_test, -scores)
+                            pr_auc = auc(recall, precision)
+                            f1 = f1_score(y_test, predictions, average='binary')
 
-                    f1_arr.append(f1)
-                    precision_arr.append(precision)
-                    recall_arr.append(recall)
-                    PR_AUC_arr.append(pr_auc)
-                    k_neighbors_arr.append(k)
-                    cv_arr.append(i)
-                    if d is not None:
-                        distance_arr.append(d.__name__)
-                    else:
-                        distance_arr.append(None)
-                    if dec is not None:
-                        decisions_arr.append(dec.__name__)
-                    else:
-                        decisions_arr.append(None)
+                            f1_arr.append(f1)
+                            precision_arr.append(precision)
+                            recall_arr.append(recall)
+                            PR_AUC_arr.append(pr_auc)
+                            k_neighbors_arr.append(k)
+                            cv_arr.append(i)
+                            if d is not None:
+                                distance_arr.append(d.__name__)
+                            else:
+                                distance_arr.append(None)
+                            if scr is not None:
+                                decisions_arr.append(scr.__name__)
+                            else:
+                                decisions_arr.append(None)
+                            filter_arr.append(filter)
+                            threshold_arr.append(th)
                 
-    result_df = pd.DataFrame({"f1":f1_arr, "precision":precision_arr, "recall":recall_arr, "PR_AUC":PR_AUC_arr, 
-                "distance":distance_arr, "K neighbors":k_neighbors_arr, "CV":cv_arr, "decision function":decisions_arr})
+    result_df = pd.DataFrame({"f1":f1_arr, "precision":precision_arr, "recall":recall_arr, 
+                              "PR_AUC":PR_AUC_arr, "distance":distance_arr, 
+                              "K neighbors":k_neighbors_arr, "CV":cv_arr, 
+                              "score function":decisions_arr, "filter inliers":filter_arr,
+                              "percentile threshold":threshold_arr})
     
     return result_df
